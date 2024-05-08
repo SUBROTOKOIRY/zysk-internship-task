06a4fb8b045a390ba16fa7f78b7cb690de1a5c27
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-05-08 22:15:45 +0530

    created the react-app server

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className="text-blue-600 text-center font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default Navbar
